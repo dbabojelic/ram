389f6b6be71f1d9c85c1506ae85f164d1cb49d09
dbabojelic <dario.bab27@gmail.com>
2020-04-17 22:52:28 +0200

    micromize; first and last N mode added

--- test/minimizer_engine_test.cpp
+++ test/minimizer_engine_test.cpp
@@ -158,5 +158,31 @@ TEST_F(RamMinimizerEngineTest, Micromize) {
   EXPECT_TRUE(o.front().strand);
 }
 
+TEST_F(RamMinimizerEngineTest, MicromizeExtend) {
+  MinimizerEngine me{15, 5};
+  me.Minimize(s.begin(), s.end());
+  auto o = me.Map(s.front(), true, true, true, 1);
+  EXPECT_EQ(1, o.size());
+  EXPECT_EQ(0, o.front().lhs_id);
+  EXPECT_EQ(80, o.front().lhs_begin);
+  EXPECT_EQ(1857, o.front().lhs_end);
+  EXPECT_EQ(1, o.front().rhs_id);
+  EXPECT_EQ(55, o.front().rhs_begin);
+  EXPECT_EQ(1881, o.front().rhs_end);
+  EXPECT_EQ(242, o.front().score);
+  EXPECT_TRUE(o.front().strand);
+
+  o = me.Map(s.front(), s.back(), true, 1);
+  EXPECT_EQ(1, o.size());
+  EXPECT_EQ(0, o.front().lhs_id);
+  EXPECT_EQ(80, o.front().lhs_begin);
+  EXPECT_EQ(1857, o.front().lhs_end);
+  EXPECT_EQ(1, o.front().rhs_id);
+  EXPECT_EQ(55, o.front().rhs_begin);
+  EXPECT_EQ(1881, o.front().rhs_end);
+  EXPECT_EQ(242, o.front().score);
+  EXPECT_TRUE(o.front().strand);
+}
+
 }  // namespace test
 }  // namespace ram
